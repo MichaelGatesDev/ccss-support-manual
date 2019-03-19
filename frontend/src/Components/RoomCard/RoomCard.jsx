1f577ade1c0b3dafca10ab99bdb375689a31d5df
Michael Gates <michaelgatesdev@gmail.com>
2019-03-19 19:16:51 -0400

    Changed card design to be be 'entire card clickable'

--- frontend/src/Components/RoomCard/RoomCard.jsx
+++ frontend/src/Components/RoomCard/RoomCard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 import LinkButton from "../LinkButton/LinkButton";
 
@@ -9,25 +10,34 @@ class RoomCard extends Component {
     componentDidMount() {
         this.setState({
         });
+        this.getCoverImage();
     }
 
-    
+    getCoverImage() {
+        var buildingName = this.props.building.internalName;
+        var number = this.props.room.number;
+        var dir = "img/buildings/" + buildingName + "/rooms/" + number + "/";
+        return dir + "cover.jpg";
+    }
+
+    getTitle() {
+        return this.props.building.officialName + " " + this.props.room.number;
+    }
 
     render() {
         return (
+
             <div className="RoomCard-Component">
-                <div className="card">
-                    <img className="card-img-top" src="img/300x200.png" alt="Card image cap" />
-                    <div className="card-body">
-                        <p className="card-title">{this.props.building.officialName} {this.props.room.number}</p>
-                        <p className="card-subtitle">{this.props.room.name ? this.props.room.name : <br />}</p>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <LinkButton
-                            title="View Details"
-                            href={`/rooms/${this.props.room._id}`}
-                        />
+                <Link to={"room/" + this.props.room._id}>
+                    <div className="card">
+                        <img className="card-img-top" src={"img/300x200.png"} alt={"Image of " + this.getTitle()} />
+                        <div className="card-body">
+                            <p className="card-title">{this.getTitle()}</p>
+                            <p className="card-subtitle">{this.props.room.name ? this.props.room.name : <br />}</p>
+                            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        </div>
                     </div>
-                </div>
+                </Link>
             </div>
         );
     }
