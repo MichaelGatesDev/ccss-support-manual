12d98cf0028d93e4256c0387a51088ad7ac07beb
Michael Gates <michaelgatesdev@gmail.com>
2019-04-03 09:02:20 -0400

    Added helpful navigation links

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -48,10 +48,10 @@ class NavBar extends Component {
                         }
                         <ul className="navbar-nav ml-auto" id="navbarNavDropdown">
                             <li className="nav-item">
-                                <a className="nav-link" href="/settings">Settings</a>
+                                <a className="nav-link" href="https://banner.plattsburgh.edu/pls/prod/psu_genweb.master_sched_search" target="_blank">Master Schedule</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/debug">Debug</a>
+                                <a className="nav-link" href="https://suny.service-now.com/navpage.do" target="_blank">ServiceNow</a>
                             </li>
                         </ul>
                     </div>
