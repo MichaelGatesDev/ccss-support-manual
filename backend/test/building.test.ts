f60cf606fe4a39334162eb88b1b9aa30bbae1fca
Michael Gates <michaelgatesdev@gmail.com>
2019-05-06 14:22:31 -0400

    Add building test

---
+++ backend/test/building.test.ts
@@ -0,0 +1,27 @@
+import { Building } from '../src/Building';
+
+let building: Building;
+let buildingOfficialName = "My Cool Building";
+let buildingNicknames: string[] = ['my', 'cool', 'building', 'mcb'];
+
+test('creates a building', () => {
+    building = new Building(
+        buildingOfficialName,
+        buildingNicknames
+    );
+});
+
+test('sets internal name', () => {
+    // expect internal name
+    expect(building.getInternalName() === buildingOfficialName.toLocaleLowerCase().replace(/\s/, '-'));
+});
+
+test('checks nicknames', () => {
+    // expect amount of nicknames
+    expect(building.getNicknames().length === buildingNicknames.length);
+});
+
+test('checks all names', () => {
+    // expect [nick]name exists
+    expect(building.hasName('mcb'));
+});
