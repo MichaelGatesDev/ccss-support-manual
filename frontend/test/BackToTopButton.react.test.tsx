5305b301edd1b7e05284b4a63fead44ca8dde27c
Michael Gates <michaelgatesdev@gmail.com>
2019-05-12 12:18:44 -0400

    Added travis (#11)
    * Added travis

---
+++ frontend/test/BackToTopButton.react.test.tsx
@@ -0,0 +1,8 @@
+/*
+import BackToTopButton from "../src/Components/BackToTopButton/BackToTopButton";
+
+import renderer from 'react-test-renderer';
+
+test('Does not appear until scrolled down', () => {
+});
+*/
