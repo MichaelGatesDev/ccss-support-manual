f07caf4cf0aed502b435e3e74f1012456fe4a060
Michael Gates <michaelgatesdev@gmail.com>
2019-08-12 18:53:44 -0400

    Use util class

--- packages/utilities/src/configuration/configuration-utils.ts
+++ packages/utilities/src/configuration/configuration-utils.ts
@@ -1,8 +1,9 @@
 import fs from "fs";
 import { ConfigBase } from "./configuration";
+import { FileUtils } from "../common";
 
 
-interface ConfigResult {
+export interface ConfigIOResult {
     wasCreated: boolean;
     loaded: ConfigBase;
 }
@@ -11,34 +12,22 @@ interface ConfigResult {
 export class ConfigUtils {
 
     public static async createIfNotExists<T extends ConfigBase>(path: string, base: { new(...args: any[]): T }, baseArgs: any[]): Promise<boolean> {
-        try {
-            await fs.promises.access(path, fs.constants.W_OK);
-            return false;
-        } catch (error) {
-            try {
-                await new base(...baseArgs).save();
-                return true;
-            } catch (error) {
-                throw error;
-            }
-        }
+        if (FileUtils.checkExists(path)) return false;
+        await new base(...baseArgs).save();
+        return true;
     }
 
     public static async load<T extends ConfigBase>(path: string, base: { new(...args: any[]): T }, baseArgs: any[]): Promise<ConfigBase> {
-        try {
-            await fs.promises.access(path, fs.constants.R_OK);
-            const rawData = await fs.promises.readFile(path, {
-                encoding: "utf8"
-            });
-            const data = JSON.parse(rawData);
-            const deserialized: ConfigBase = new base(...baseArgs).deserialize(data);
-            return deserialized;
-        } catch (error) {
-            throw error;
-        }
+        if (FileUtils.checkExists(path)) throw new Error(`Can not load configuration because it does not exist: ${path}`);
+        const rawData = await fs.promises.readFile(path, {
+            encoding: "utf8"
+        });
+        const data = JSON.parse(rawData);
+        const deserialized: ConfigBase = new base(...baseArgs).deserialize(data);
+        return deserialized;
     }
 
-    public static async createIfNotExistsAndLoad<T extends ConfigBase>(path: string, base: { new(...args: any[]): T }, baseArgs: any[]): Promise<ConfigResult> {
+    public static async createIfNotExistsAndLoad<T extends ConfigBase>(path: string, base: { new(...args: any[]): T }, baseArgs: any[]): Promise<ConfigIOResult> {
         const wasCreated = await ConfigUtils.createIfNotExists(path, base, baseArgs);
         const loadedConfigBase = await ConfigUtils.load(path, base, baseArgs);
         return { wasCreated: wasCreated, loaded: loadedConfigBase };
