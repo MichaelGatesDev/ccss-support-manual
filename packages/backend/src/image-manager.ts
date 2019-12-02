6e207a4cb4f574c7897b7897253ca02d29552628
Michael Gates <michaelgatesdev@gmail.com>
2019-12-02 07:47:54 -0500

    Use regex to replace globally, escape separator

--- packages/backend/src/image-manager.ts
+++ packages/backend/src/image-manager.ts
@@ -126,7 +126,7 @@ export class ImageManager {
             const image = new BuildingImageFactory(
                 new ImageFactory()
                     .ofType(type)
-                    .withPath(newPath.replace(new RegExp(path.sep, 'g'), "/"))
+                    .withPath(newPath.replace(new RegExp(escape(path.sep), 'g'), "/"))
                     .withActualPath(filePath)
                     .withThumb({ fileName, path: `${newPath}.thumb.jpg` })
                     .build()
@@ -152,7 +152,7 @@ export class ImageManager {
             const image = new RoomImageFactory(
                 new ImageFactory()
                     .ofType(type)
-                    .withPath(newPath.replace(new RegExp(path.sep, 'g'), "/"))
+                    .withPath(newPath.replace(new RegExp(escape(path.sep), 'g'), "/"))
                     .withActualPath(filePath)
                     .withThumb({ fileName, path: `${newPath}.thumb.jpg` })
                     .build()
