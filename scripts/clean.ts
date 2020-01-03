4ceba7ff18b7f86fe47656195a0305d7a1ef75d6
Michael Gates <michaelgatesdev@gmail.com>
2020-01-03 22:41:58 +0700

    Dev dependency update

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -1,10 +1,7 @@
 import rimraf from "rimraf";
 
-console.log("==== PERFORMING CLEANUP ====");
-
+console.log("Performing cleanup...");
+console.log("");
 rimraf.sync("tsconfig.tsbuildinfo");
 rimraf.sync("packages/*/{tsconfig.tsbuildinfo,lib,dist,build,application-*}");
 rimraf.sync("build");
-
-console.log("==== CLEANUP COMPLETE ====");
-console.log("");
