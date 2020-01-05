d8e6976e0df9171b8e56ca5a1cbff515be9e012c
Michael Gates <michaelgatesdev@gmail.com>
2020-01-05 13:29:15 +0700

    Minor changes to test descriptions to match conventions

--- packages/backend/src/__tests__/buildings.test.ts
+++ packages/backend/src/__tests__/buildings.test.ts
@@ -11,23 +11,20 @@ let building: Building;
 const buildingOfficialName = "My Cool Building";
 const buildingNicknames: string[] = ["my", "cool", "building", "mcb"];
 
-test("creates a building", (): void => {
-
+test("shouuld create a building", (): void => {
     building = new BuildingFactory()
         .withOfficialName(buildingOfficialName)
         .withInternalName(StringUtils.internalize(buildingOfficialName))
         .withNicknames(buildingNicknames)
         .build();
-
-    expect(building);
+    expect(building).toBeDefined();
 });
 
-test("sets internal name", (): void => {
-    // expect internal name
+test("should set the building's internal name", (): void => {
     expect(building.internalName === StringUtils.internalize(building.officialName));
 });
 
-test("checks nicknames", (): void => {
+test("should validate the building's nicknames", (): void => {
     // expect amount of nicknames
     expect(building.nicknames.length === buildingNicknames.length);
 
@@ -37,29 +34,29 @@ test("checks nicknames", (): void => {
     expect(BuildingUtils.hasName(building, randomNick!));
 });
 
-test('should add the building to the building manager', () => {
+test("should add the building to the building manager", () => {
     expect(buildingManager.getBuildingByName(building.internalName)).toBeUndefined();
     expect(buildingManager.addBuilding(building)).toBe(true);
     expect(buildingManager.getBuildingByName(building.internalName)).toBeDefined();
 });
 
-test('should not add duplicate buildings', () => {
+test("should not add duplicate buildings", () => {
     const clone = Object.assign<{}, Building>({}, building);
     expect(building).toEqual(clone);
     expect(buildingManager.addBuilding(clone)).toBe(false);
 });
 
-test('should remove building', () => {
+test("should remove building", () => {
     expect(buildingManager.removeBuilding(building)).toBe(true);
 });
 
-test('should clear all buildings', () => {
+test("should clear all buildings", () => {
     buildingManager.clear();
     expect(buildingManager.buildings.length).toBe(0);
 });
 
 
-test('should add multiple buildings', () => {
+test("should add multiple buildings", () => {
     const a = new BuildingFactory().withOfficialName("building a").build();
     const b = new BuildingFactory().withOfficialName("building b").build();
     const c = new BuildingFactory().withOfficialName("building c").build();
