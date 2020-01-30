bee9ffe3ce05eebdb7cfe6ab15300ff93f391099
Michael Gates <michaelgatesdev@gmail.com>
2020-01-30 17:43:49 -0500

    Possible reference fix

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -2,6 +2,4 @@ import rimraf from "rimraf";
 
 console.log("Performing cleanup...");
 console.log("");
-rimraf.sync("tsconfig.tsbuildinfo");
-rimraf.sync("packages/*/{tsconfig.tsbuildinfo,lib,dist,build,application-*}");
-rimraf.sync("build");
+rimraf.sync("packages/*/{*.tsbuildinfo,lib,dist,build,application-*}");
