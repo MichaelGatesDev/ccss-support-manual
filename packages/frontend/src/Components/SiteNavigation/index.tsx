bab5493ce7846ce15722961db3b8d8db9cdf1493
Michael Gates <michaelgatesdev@gmail.com>
2020-03-30 12:48:52 -0400

    Add external links

--- packages/frontend/src/Components/SiteNavigation/index.tsx
+++ packages/frontend/src/Components/SiteNavigation/index.tsx
@@ -47,17 +47,29 @@ export const SiteNavigation = (props: Props): JSX.Element => {
         <Navbar fixed="top" id="site-nav">
           <Nav className="mr-auto">
             <Nav.Item>
-              <Nav.Link as={Link} to="/">
+              <Nav.Link
+                href="https://banner.plattsburgh.edu/pls/prod/psu_genweb.master_sched_search"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Master Schedule
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={Link} to="/">
+              <Nav.Link
+                href="https://booking.plattsburgh.edu/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Room Schedule
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={Link} to="/">
+              <Nav.Link
+                href="https://suny.service-now.com/navpage.do"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 ServiceNow
               </Nav.Link>
             </Nav.Item>
