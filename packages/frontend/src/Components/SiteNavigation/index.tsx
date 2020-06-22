9f916e73d054577289cf63c567ec0fadb9d4bf53
Michael Gates <michaelgatesdev@gmail.com>
2020-06-21 21:32:59 -0400

    Update navigation links

--- packages/frontend/src/Components/SiteNavigation/index.tsx
+++ packages/frontend/src/Components/SiteNavigation/index.tsx
@@ -4,46 +4,37 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import externalNavRoutes from "../../external-nav-routes.json";
+import internalNavRoutes from "../../internal-nav-routes.json";
+
 interface Props {}
 
 export const SiteNavigation = (): JSX.Element => {
   return (
     <Navbar fixed="top" id="site-nav">
       <Nav className="mr-auto">
-        <Nav.Item>
-          <Nav.Link
-            href="https://banner.plattsburgh.edu/pls/prod/psu_genweb.master_sched_search"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Master Schedule
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            href="https://booking.plattsburgh.edu/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Room Schedule
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            href="https://suny.service-now.com/navpage.do"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ServiceNow
-          </Nav.Link>
-        </Nav.Item>
+        {Object.keys(internalNavRoutes).map(route => {
+          const href = internalNavRoutes[route];
+          return (
+            <Nav.Item>
+              <Nav.Link as={Link} to={href}>
+                {route.replace(/\_/, " ")}
+              </Nav.Link>
+            </Nav.Item>
+          );
+        })}
       </Nav>
       <Nav>
-        <Nav.Item>
-          <Nav.Link as={Link} to="/settings">
-            <small>Settings</small>
-          </Nav.Link>
-        </Nav.Item>
+        {Object.keys(externalNavRoutes).map(route => {
+          const href = externalNavRoutes[route];
+          return (
+            <Nav.Item>
+              <Nav.Link href={href} target="_blank" rel="noopener noreferrer">
+                {route.replace(/\_/, " ")}
+              </Nav.Link>
+            </Nav.Item>
+          );
+        })}
       </Nav>
     </Navbar>
   );
